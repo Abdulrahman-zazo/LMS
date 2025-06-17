0f781b28d50236cae05993943c77388bb9c7fa4c
Abdulrahman-zazo <abdulrahmanzazo@outlook>
2025-06-17 15:13:02 +0300

    final build

--- src/components/Curricula.tsx
+++ src/components/Curricula.tsx
@@ -208,7 +208,7 @@ const Curricula = () => {
                 <img
                   src={subject.image}
                   alt={subject.name}
-                  className="w-full object-cover  h-full"
+                  className="w-full object-cover  h-[200px]"
                   onError={(e) => {
                     e.currentTarget.src =
                       "https://placehold.co/100x60/F0F4F8/000000?text=N/A";
