8aa7c110082d83903b1a820b4fa9703b711b78fb
Abdulrahman-zazo <abdulrahmanzazo@outlook>
2025-07-14 02:34:27 +0300

    fix: update vercel config

--- dist/assets/ScrollToHash-D8u1WM41.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r}from"./index-BIRCEoYC.js";const a=()=>{const o=e();return r.useLayoutEffect(()=>{const t=o.hash;if(t){const s=document.querySelector(t);s&&setTimeout(()=>{s.scrollIntoView({behavior:"smooth",block:"start"})},100)}},[o]),null};export{a as S};
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  base: "./",
   server: {
     headers: {
       "X-Content-Type-Options": "nosniff",
