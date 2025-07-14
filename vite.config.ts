359a8e6b2fc08fb6368135536458a8ee123c2d1a
Abdulrahman-zazo <abdulrahmanzazo@outlook>
2025-07-14 18:17:12 +0300

    Add sitemap.xml to public for SEO 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: "https://h-platform.online", // استبدل هذا برابط موقعك
+      // يمكنك إضافة المزيد من الخيارات هنا مثل:
+      // exclude: ['/secret'], // استبعاد مسارات معينة
+      // routes: ['/some-static-page'], // إضافة مسارات ثابتة إضافية
+    }),
+
+    tailwindcss(),
+  ],
   build: {
     outDir: "dist",
   },
